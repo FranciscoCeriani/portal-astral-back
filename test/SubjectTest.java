bef0360ef961518d96b0581eb0e0002bdf01ab17
Tomás Shilton <tomas.shilton@ing.austral.edu.ar>
2018-11-07 01:19:18 -0300

    Added getAllSubjects test

--- test/SubjectTest.java
+++ test/SubjectTest.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.core.type.TypeReference;
+import junit.framework.Assert;
 import models.Student;
 import models.Subject;
 import org.junit.After;
@@ -37,7 +38,7 @@ public class SubjectTest {
     public void insertTest() throws Exception {
         ArrayList<String> requiredSubjects = new ArrayList<>();
         ArrayList<Student> students = new ArrayList<>();
-        Subject subject = new Subject("lab2", 3, requiredSubjects, students);
+        Subject subject = new Subject("lab2", 3, requiredSubjects);
 
         Result result = insertSubject(subject);
         assertEquals(201, result.status());
@@ -63,7 +64,7 @@ public class SubjectTest {
 
         requiredSubjects.remove(0);
         requiredSubjects.add(id);
-        Subject subject2 = new Subject("analisis", 1, requiredSubjects, students);
+        Subject subject2 = new Subject("analisis", 1, requiredSubjects);
         result = insertSubject(subject2);
         assertEquals(201, result.status());
 
@@ -79,7 +80,7 @@ public class SubjectTest {
     public void deleteTest() throws Exception {
         ArrayList<String> requiredSubjects = new ArrayList<>();
         ArrayList<Student> students = new ArrayList<>();
-        Subject subject = new Subject("lab2", 3, requiredSubjects, students);
+        Subject subject = new Subject("lab2", 3, requiredSubjects);
 
         Result result = insertSubject(subject);
         String id = contentAsString(result);
@@ -108,6 +109,48 @@ public class SubjectTest {
         result = getAllSubjects();
         subjects = readValue(result, new TypeReference<List<Subject>>(){});
         assertEquals(0, subjects.size());
+    }
+
+    @Test
+    public void getAllSubjectsTest() throws Exception{
+
+        ArrayList<String> requiredSubjects = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        Subject subject = new Subject("lab2", 3, requiredSubjects);
+        Subject subject2 = new Subject("lab3", 4, requiredSubjects);
+
+        Result result = getAllSubjects();
+        assertEquals(200, result.status());
+        List<Subject> subjects = readValue(result, new TypeReference<List<Subject>>(){});
+        assertTrue(subjects.isEmpty());
+
+        result = insertSubject(subject);
+        String subject1Id = contentAsString(result);
+
+        result = getAllSubjects();
+        assertEquals(200, result.status());
+        subjects = readValue(result, new TypeReference<List<Subject>>(){});
+        assertEquals(subjects.size(), 1);
+
+        result = insertSubject(subject2);
+        String subject2Id = contentAsString(result);
+
+        requiredSubjects.add(subject1Id);
+        Subject subject3 = new Subject("lab4", 5, requiredSubjects);
+
+        result = insertSubject(subject3);
+        String subject3Id = contentAsString(result);
+
+        result = getAllSubjects();
+        assertEquals(200, result.status());
+        subjects = readValue(result, new TypeReference<List<Subject>>(){});
+        assertEquals(subjects.size(), 3);
+
+        assertEquals(subjects.get(0).id, subject1Id);
+        assertEquals(subjects.get(1).id, subject2Id);
+        assertEquals(subjects.get(2).id, subject3Id);
+        assertEquals(subjects.get(2).requiredSubjects.get(0), subject1Id);
+        assertEquals(subjects.get(2).requiredSubjects.size(), 1);
     }
 
 
